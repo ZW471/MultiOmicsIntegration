03cd4caee0b9ded9a3e539263942cdb8090f26db
Zhiyu Wang <zw471@cam.ac.uk>
2025-03-08 19:49:57 +0000

    fixing random seeds for reproducibility

--- model/prototype_model.py
+++ model/prototype_model.py
@@ -9,6 +9,16 @@ import scanpy as sc
 # print(protein_data)
 # print(rna_data)
 #%%
+
+import random, numpy as np, torch
+random.seed(42)
+np.random.seed(42)
+torch.manual_seed(42)
+if torch.cuda.is_available():
+    torch.cuda.manual_seed_all(42)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+
 import scanpy as sc
 
 modalities = ["ATAC", "RNA"]
